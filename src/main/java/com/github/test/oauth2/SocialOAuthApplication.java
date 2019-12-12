bba238f8f1f47ad4da59e85f74182a4abb89f3ca
g521885 <Kush.GUPTA@idemia.com>
2019-12-12 17:15:56 +0530

    google+github login added

---
+++ src/main/java/com/github/test/oauth2/SocialOAuthApplication.java
@@ -0,0 +1,13 @@
+package com.github.test.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialOAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialOAuthApplication.class, args);
+	}
+
+}
